db8ee0da967d6561b3993508c935d3b122ec98cc
Gabriel Alves Silva <bielvipbh@hotmail.com>
2020-07-18 14:59:52 -0300

    Commit inicial

---
+++ src/main/java/com/unkapps/leilao/LeilaoApplication.java
@@ -0,0 +1,30 @@
+package com.unkapps.leilao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class LeilaoApplication {
+    private static final Logger log = LoggerFactory.getLogger(LeilaoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(LeilaoApplication.class);
+        app.setDefaultProperties(Map.of(
+                "spring.profiles.default", "dev"
+        ));
+
+        Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\n" +
+                        "Aplicação executando!: http://localhost:{}" +
+                        "\n\n----------------------------------------------------------",
+                env.getProperty("server.port")
+        );
+
+    }
+
+}
---
+++ src/test/java/com/unkapps/leilao/LeilaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unkapps.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
