f6075c3ce3b62caa52f8b4ea6cc3869abba0f974
Gabriel Alves Silva <bielvipbh@hotmail.com>
2020-07-20 01:50:30 -0300

    403 when token expires

--- api/src/main/java/com/unkapps/leilao/filter/JwtRequestFilter.java
+++ api/src/main/java/com/unkapps/leilao/filter/JwtRequestFilter.java
@@ -1,7 +1,9 @@
 package com.unkapps.leilao.filter;
 
 import com.unkapps.leilao.service.auth.jwt.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -33,11 +35,15 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String username = null;
         String jwt = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            username = jwtService.extractUsername(jwt);
+        try {
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                jwt = authorizationHeader.substring(7);
+                username = jwtService.extractUsername(jwt);
+            }
+        } catch (ExpiredJwtException err) {
+            response.sendError(HttpStatus.FORBIDDEN.value());
+            return;
         }
-
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
