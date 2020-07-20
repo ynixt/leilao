6da696766a80452747b6194cb9c598fa862776ea
Gabriel Alves Silva <bielvipbh@hotmail.com>
2020-07-20 00:46:37 -0300

    Delete bids on delete auction

--- api/src/main/java/com/unkapps/leilao/repository/BidRepository.java
+++ api/src/main/java/com/unkapps/leilao/repository/BidRepository.java
@@ -5,6 +5,7 @@ import com.unkapps.leilao.domain.Bid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -19,4 +20,8 @@ public interface BidRepository extends JpaRepository<Bid, Long> {
                     " where bid.auction.id = :oidAuction"
     )
     Page<Bid> list(Pageable page, @NotNull @Param("oidAuction") Long oidAuction);
+
+    @Modifying
+    @Query("delete from Bid where auction.id = :auctionId")
+    int deleteByAuctionId(@Param("auctionId")Long auctionid);
 }
--- api/src/main/java/com/unkapps/leilao/service/auction/AuctionDeleteService.java
+++ api/src/main/java/com/unkapps/leilao/service/auction/AuctionDeleteService.java
@@ -2,9 +2,11 @@ package com.unkapps.leilao.service.auction;
 
 import com.unkapps.leilao.domain.Auction;
 import com.unkapps.leilao.repository.AuctionRepository;
+import com.unkapps.leilao.repository.BidRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 
 @Service
@@ -15,8 +17,14 @@ public class AuctionDeleteService {
     @Autowired
     private AuctionService auctionService;
 
+    @Autowired
+    private BidRepository bidRepository;
+
+    @Transactional
     public void deleteById(@NotNull Long id) {
         Auction auction = auctionService.getAuctionForModify(id);
+
+        bidRepository.deleteByAuctionId(id);
 
         auctionRepository.delete(auction);
     }
